c3fadbfabc1a443ba8eb6578843757269cf90eab
Bushra01-Dhaka <humayraanjum87@gmail.com>
2023-10-20 14:06:15 +0600

    for updating specified id's data, PUT operation is applied

--- index.js
+++ index.js
@@ -71,6 +71,30 @@ async function run() {
       res.send(result);
     })
 
+    // 5. updating
+
+    app.put('/products/:id', async(req,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+
+      const product = {
+        $set: {
+          name : updatedProduct.name, 
+          brand :updatedProduct.brand, 
+          type : updatedProduct.type, 
+          rating : updatedProduct.rating, 
+          image : updatedProduct.image, 
+          description : updatedProduct.description, 
+          price: updatedProduct.price, 
+        }
+      }
+      const result = await productCollection.updateOne(filter, product, options);
+      res.send(result);
+
+    })
+
     
 
    
