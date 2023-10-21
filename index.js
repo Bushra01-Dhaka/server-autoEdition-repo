2c52d881318d6ef3c45257135a218dfa7bcc3f93
Bushra01-Dhaka <humayraanjum87@gmail.com>
2023-10-21 13:05:40 +0600

    Created vercel.json

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     //2.
     const productCollection = client.db("productDB").collection("products");
@@ -120,12 +120,8 @@ async function run() {
 
     
 
-   
-
-
-
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
